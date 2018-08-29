eaecd84ee39384380d0ade89c1c777be10c2b21a
Jorge Lanza <jlanza@tlmat.unican.es>
2018-08-29 20:44:53 +0200

    Enable configuration

--- src/main/java/es/unican/tlmat/smartsantander/big_iot/provider/App.java
+++ src/main/java/es/unican/tlmat/smartsantander/big_iot/provider/App.java
@@ -1,20 +1,33 @@
 package es.unican.tlmat.smartsantander.big_iot.provider;
 
 import java.io.IOException;
+import java.lang.invoke.MethodHandles;
 import java.util.Scanner;
 
 import org.eclipse.bigiot.lib.exceptions.IncompleteOfferingDescriptionException;
 import org.eclipse.bigiot.lib.exceptions.NotRegisteredException;
 import org.eclipse.bigiot.lib.misc.BridgeIotProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import es.unican.tlmat.smartsantander.big_iot.provider.fiware.OrionHttpClient;
+import es.unican.tlmat.smartsantander.big_iot.provider.offerings.AirPollutionSensorsOffering;
+import es.unican.tlmat.smartsantander.big_iot.provider.offerings.AtmosphericPressureSensorOffering;
 import es.unican.tlmat.smartsantander.big_iot.provider.offerings.BikeSharingStationsOffering;
+import es.unican.tlmat.smartsantander.big_iot.provider.offerings.BusArrivalEstimationOffering;
+import es.unican.tlmat.smartsantander.big_iot.provider.offerings.NoiseSensorsOffering;
 import es.unican.tlmat.smartsantander.big_iot.provider.offerings.ParkingSpaceAvailabilityOffering;
+import es.unican.tlmat.smartsantander.big_iot.provider.offerings.RelativeHumiditySensorsOffering;
+import es.unican.tlmat.smartsantander.big_iot.provider.offerings.TemperatureSensorsOffering;
+import es.unican.tlmat.smartsantander.big_iot.provider.offerings.TrafficConditionsOffering;
+import es.unican.tlmat.smartsantander.big_iot.provider.offerings.WindSensorsOffering;
 
 /**
  * Hello world!.
  *
  */
 public class App {
+  private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
   public static void main(String[] args) throws InterruptedException,
       IncompleteOfferingDescriptionException, IOException, NotRegisteredException {
@@ -22,12 +35,41 @@ public class App {
     // Load example properties file
     BridgeIotProperties prop = BridgeIotProperties.load("smartsantander.properties");
 
+    Configuration config = Configuration.load("smartsantander.properties");
+
     Provider smsProvider = new Provider(prop);
+
+    OrionHttpClient orion = new OrionHttpClient(config.getOrionUrl());
 
     smsProvider.start();
 
-    smsProvider.registerOffering(ParkingSpaceAvailabilityOffering.getInstance());
-    smsProvider.registerOffering(BikeSharingStationsOffering.getInstance());
+    smsProvider.registerOffering(ParkingSpaceAvailabilityOffering.create(orion));
+    smsProvider.registerOffering(BikeSharingStationsOffering.create(orion));
+    smsProvider.registerOffering(TrafficConditionsOffering.create(orion));
+    smsProvider.registerOffering(BusArrivalEstimationOffering.create(orion));
+    smsProvider.registerOffering(NoiseSensorsOffering.create(orion));
+    smsProvider.registerOffering(TemperatureSensorsOffering.create(orion));
+    smsProvider.registerOffering(RelativeHumiditySensorsOffering.create(orion));
+    smsProvider.registerOffering(WindSensorsOffering.create(orion));
+    smsProvider.registerOffering(AtmosphericPressureSensorOffering.create(orion));
+    smsProvider.registerOffering(AirPollutionSensorsOffering.create(orion));
+
+    final Thread mainThread = Thread.currentThread();
+    Runtime.getRuntime().addShutdownHook(new Thread() {
+      @Override
+      public void run() {
+        log.info("Shutdown hook running for graceful shutdown");
+        // Deregister the Offering from the Marketplace
+        smsProvider.stop();
+
+        try {
+          mainThread.join();
+        } catch (InterruptedException e) {
+          log.error("Shutdown", e);
+          Thread.currentThread().interrupt();
+        }
+      }
+    });
 
     // Run until user presses the ENTER key
     System.out.println(">>>>>>  Terminate SmartSantander Provider by pressing ENTER  <<<<<<");
@@ -36,27 +78,6 @@ public class App {
     keyboard.close();
 
     smsProvider.stop();
-
-//    final Thread mainThread = Thread.currentThread();
-//    Runtime.getRuntime().addShutdownHook(new Thread() {
-//      @Override
-//      public void run() {
-//        System.out.println("Shutdown hook running for graceful shutdown");
-//        // Deregister the Offering from the Marketplace
-//        provider.deregister(offeringDescription);
-//
-//        // Terminate the Provider instance
-//        provider.terminate();
-//
-//        try {
-//          mainThread.join();
-//        } catch (InterruptedException e) {
-//          // TODO Auto-generated catch block
-//          e.printStackTrace();
-//        }
-//      }
-//    });
-
   }
 
 }
---
+++ src/main/java/es/unican/tlmat/smartsantander/big_iot/provider/Configuration.java
@@ -0,0 +1,64 @@
+package es.unican.tlmat.smartsantander.big_iot.provider;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Configuration {
+
+  private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+  private static final String ORION_URL = "orion";
+
+  Properties props;
+
+  private Configuration(final String path) throws IOException {
+    props = new Properties();
+
+    try (FileInputStream fs = new FileInputStream(new File(path))) {
+      props.load(fs);
+    }
+
+    log.info("Configuration read from file '" + path + "'");
+  }
+
+  public static Configuration load(final String path) throws IOException {
+    return new Configuration(path);
+  }
+
+  /**
+   * Gets the requested property
+   *
+   * @param key
+   *            The key of the property
+   * @param defaultValue
+   *            The default value for the requested key
+   *
+   * @return the requested property
+   */
+  private String getProperty(String key, String defaultValue) {
+    return props.getProperty(key, defaultValue);
+  }
+
+  /**
+   * Gets the requested property
+   *
+   * @param key
+   *            The key of the property
+   *
+   * @return the requested property
+   */
+  private String getProperty(String key) {
+    return props.getProperty(key);
+  }
+
+  public String getOrionUrl() {
+    return getProperty(ORION_URL);
+  }
+
+}
