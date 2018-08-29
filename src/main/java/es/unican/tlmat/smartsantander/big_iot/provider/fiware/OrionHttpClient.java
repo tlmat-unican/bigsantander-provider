532f95defa6e997ea71c6823d1e463b7d6909505
Jorge Lanza <jlanza@tlmat.unican.es>
2018-08-29 19:13:42 +0200

    Change to OkHTTP

--- src/main/java/es/unican/tlmat/smartsantander/big_iot/provider/fiware/OrionHttpClient.java
+++ src/main/java/es/unican/tlmat/smartsantander/big_iot/provider/fiware/OrionHttpClient.java
@@ -1,11 +1,9 @@
 package es.unican.tlmat.smartsantander.big_iot.provider.fiware;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.lang.invoke.MethodHandles;
 import java.net.HttpURLConnection;
-import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,12 +12,39 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 
+import okhttp3.CacheControl;
+import okhttp3.HttpUrl;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class OrionHttpClient {
   private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
   private static String USER_AGENT = "BIG IoT/1.0 BigSantander Provider/1.0";
+  private static final MediaType MEDIA_TYPE_JSON = MediaType
+      .parse("application/json; charset=utf-8");
 
-  private static final ObjectMapper mapper = new ObjectMapper();
+  private static final OkHttpClient client = new OkHttpClient.Builder()
+      .connectTimeout(10, TimeUnit.SECONDS).writeTimeout(10, TimeUnit.SECONDS)
+      .readTimeout(30, TimeUnit.SECONDS)
+//      .cache(null)
+      .build();
+
+  private static final HttpUrl configUrl = HttpUrl
+      .parse("http://orion-cb.tlmat.synchronicity-iot.eu:1026/v2/op/query");
+
+  private static final HttpUrl defaultOrionUrl = new HttpUrl.Builder().scheme(configUrl.scheme())
+      .host(configUrl.host()).port(configUrl.port()).addPathSegment("v2")
+      .addQueryParameter("limit", "1000").addQueryParameter("options", "count").build();
+
+  private static final Request defaultRequest = new Request.Builder()
+      .addHeader("User-Agent", USER_AGENT).cacheControl(CacheControl.FORCE_NETWORK)
+      .url(defaultOrionUrl).build();
+
+  protected static final ObjectMapper mapper = new ObjectMapper();
 
   private final String orionHost;
 
@@ -27,71 +52,47 @@ public class OrionHttpClient {
     this.orionHost = url;
   }
 
-  private HttpURLConnection makeQueryRequest(Query query) throws Exception {
-    String queryParams = "options=keyValues,count&limit=1000";
-    String url = orionHost.concat("?").concat(queryParams);
-    URL obj = new URL(url);
-    HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
-    conn.setRequestMethod("POST");
-    conn.setReadTimeout(10000);
-    conn.setConnectTimeout(15000);
-    conn.setDoInput(true);
-    conn.setDoOutput(true);
+  // IOException when there is a connection error or similar and should be translate to HTTP error 500
+  public ArrayNode postQuery(Query query) throws IOException {
+    HttpUrl url = defaultOrionUrl.newBuilder().addPathSegments("op/query")
+        .addQueryParameter("options", "keyValues").build();
 
-    log.debug("adding headers");
+    RequestBody body = RequestBody.create(MEDIA_TYPE_JSON, mapper.writeValueAsString(query));
+    Request request = defaultRequest.newBuilder().url(url).post(body)
+        .addHeader("Accept", "application/json").build();
 
-    // Add request headers
-    conn.setRequestProperty("User-Agent", USER_AGENT);
-    conn.setRequestProperty("Content-Type", "application/json");
-    conn.setRequestProperty("Accept", "application/json");
-
-    // Write to output stream
-    OutputStream out = conn.getOutputStream();
-    mapper.writer().writeValue(out, query);
-    // Meter en finally
-    out.flush();
-
-    log.debug("Connection sent");
-
-    return conn;
-  }
-
-  private ArrayNode processQueryResponse(HttpURLConnection conn) throws IOException {
-    int responseCode = conn.getResponseCode();
-    log.debug("Response code" + responseCode);
-    if (responseCode != HttpURLConnection.HTTP_OK) {
-      // In case we want to capture the error message from the server
-      // Orion returns a JSON document:
-      // {
-      // "error": "BadRequest",
-      // "description": "Invalid value for URI param /options/"
-      // }
-      // InputStream stream = connection.getErrorStream();
-      throw new IOException("HTTP error " + responseCode);
+    Response response = client.newCall(request).execute();
+    if (response.code() != HttpURLConnection.HTTP_OK) {
+      // Orion returns a JSON document
+      throw new IOException(
+          "HTTP error " + response.code() + "from Orion server: " + response.body().string());
     }
 
-    // Just in case the below code doesn't work
-    // try (BufferedReader reader = new BufferedReader(
-    // new InputStreamReader(myURLConnection.getInputStream()))) {
-    // reader.lines().forEach(System.out::println);
-    // }
-    log.debug("Start parsing output");
-    try {
-      InputStream in = conn.getInputStream();
-      final JsonNode fiwareNodes = mapper.reader().readTree(in);
-      if (fiwareNodes.isArray()) {
-        return (ArrayNode)fiwareNodes;
-      } else {
-        throw new IOException("Not expected response from Orion server.");
-      }
-    } catch (IOException e) {
-      throw new IOException(e);
+    final JsonNode fiwareNodes = mapper.reader().readTree(response.body().byteStream());
+    if (fiwareNodes.isArray()) {
+      return (ArrayNode) fiwareNodes;
+    } else {
+      throw new IllegalArgumentException("Expecting JSON array from Orion server");
     }
   }
 
-  public ArrayNode sendQuery(Query query) throws Exception {
-    HttpURLConnection conn = makeQueryRequest(query);
-    return processQueryResponse(conn);
-  }
+  public String getEntityAttributeValue(String entityId, String attribute) throws IOException {
+    HttpUrl url = defaultOrionUrl.newBuilder()
+        .addPathSegment("entities")
+        .addPathSegment(entityId)
+        .addPathSegment("attrs")
+        .addPathSegment(attribute)
+        .addPathSegment("value")
+        .build();
 
+    Request request = defaultRequest.newBuilder()
+        .url(url)
+        .get()
+        .build();
+
+    Response response = client.newCall(request).execute();
+    String name = response.body().string();
+    // Trim double and single quotes from beginning and end
+    return name.replaceAll("^[\"']+|[\"']+$", "");
+  }
 }
